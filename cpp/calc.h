2f5ffb98cfbaf755bc4540c1d8d942cb473c85e8
e4rthh <pannawat.sripor@gmail.com>
2026-08-10 15:22:34 +0700

    Delete cpp/calc.h

--- cpp/calc.h
+++
@@ -1,7 +0,0 @@
-#pragma once               //tells the copiler to compile once
-#include <vector>
-#include <string>
-
-std::vector<std::string> tokenize(const std::string& expr);
-std::vector<std::string> shuting_yard(const std::vector<std::string>& tokens);
-double evaluvation(const std::vector<std::string>& tokens);                     //header file
