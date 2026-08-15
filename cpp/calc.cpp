6f9dcc7d783a0dc0c09935840b6cca93c95b4b72
e4rthh <pannawat.sripor@gmail.com>
2026-08-15 17:32:15 +0700

    decimal added

--- cpp/calc.cpp
+++ cpp/calc.cpp
@@ -1,132 +1,164 @@
-#include "calc.h"                   //function files
+#include <cctype>
+#include <cmath>
 #include <iostream>
+#include <stack>
 #include <string>
 #include <vector>
-#include <cctype>
-#include <stack>
-#include <cmath>
 
-int precedence(const std::string& op) {
-    if (op == "+" || op == "-") return 1; //order of operation so + - is lower than * /
-    if (op == "*" || op == "/") return 2; 
-    if (op == "^") return 3;
-    if (op == "sqrt") return 4;
-    return 0;
+int precedence(const std::string &op) {
+  if (op == "+" || op == "-")
+    return 1; // order of operation so + - is lower than * /
+  if (op == "*" || op == "/")
+    return 2;
+  if (op == "^")
+    return 3;
+  if (op == "sqrt")
+    return 4;
+  return 0;
 }
 
-bool is_operator(const std::string& t) {
-    return t == "+" || t == "-" || t == "*" || t == "/" || t == "^" || t == "sqrt";
+bool is_operator(const std::string &t) {
+  return t == "+" || t == "-" || t == "*" || t == "/" || t == "^" ||
+         t == "sqrt";
 }
 
-bool isltr(const std::string& t) {
-       return t != "^";
-}
+bool isltr(const std::string &t) { return t != "^"; }
 
-std::vector<std::string> tokenize(const std::string& expr) { //function stdvector string for outputing as a vector of string,t he const std::string& expr for const std::string it just says i wont do anything to the string and we input the variable as expr
-    std::vector<std::string> tokens;   //tokens vector string
-    std::string current;   //string for current to pop
+std::vector<std::string>
+tokenize(const std::string
+             &expr) { // function stdvector string for outputing as a vector of
+                      // string,t he const std::string& expr for const
+                      // std::string it just says i wont do anything to the
+                      // string and we input the variable as expr
+  std::vector<std::string> tokens; // tokens vector string
+  std::string current;             // string for current to pop
 
-    for (size_t i = 0; i < expr.size(); ) {  //loop for charecter in expr (aka string)
-        char c = expr[i];  //changed to this because we can now check in the strings instead of by letters like sqrt(4) wil now be [sqrt(4)] in which we can easily check using to compare string
-        while (i < expr.size() && std::isdigit(expr[i])) {    //check numbers faster
-             current += expr[i];
-                i++;
-            }
-            if (!current.empty()) {   // if a symbol
-                tokens.push_back(current);   //push the current number to the token 
-                current.clear();
-                continue;  //clear the string using continue to avoid having 4 be checked again in the if below
-            }
-            if (expr.substr(i, 4) == "sqrt") {
-                 tokens.push_back("sqrt");
-                 i += 4;
-                 continue;
-}
-            if (c != ' ') {      
-                tokens.push_back(std::string(1, c));//push the symbol in 1, c stands for string of 1 charecter
-                i++;  
-            }
-        }
-    if (!current.empty()) {      //for leftovers to push to tokens
-        tokens.push_back(current);
+  for (size_t i = 0;
+       i < expr.size();) { // loop for charecter in expr (aka string)
+    char c = expr[i]; // changed to this because we can now check in the strings
+                      // instead of by letters like sqrt(4) wil now be [sqrt(4)]
+                      // in which we can easily check using to compare string
+    if (std::isdigit(static_cast<unsigned char>(expr[i])) || expr[i] == '.') {
+      while (i < expr.size() &&
+             (std::isdigit(static_cast<unsigned char>(expr[i])) ||
+              expr[i] == '.')) {
+        current += expr[i];
+        i++;
+      }
     }
-
-    return tokens;
+    if (!current.empty()) {      // if a symbol
+      tokens.push_back(current); // push the current number to the token
+      current.clear();
+      continue; // clear the string using continue to avoid having 4 be checked
+                // again in the if below
+    }
+    if (expr.substr(i, 4) == "sqrt") {
+      tokens.push_back("sqrt");
+      i += 4;
+      continue;
+    }
+    if (c != ' ') {
+      tokens.push_back(std::string(
+          1, c)); // push the symbol in 1, c stands for string of 1 charecter
+      i++;
+    }
   }
 
-std::vector<std::string> shuting_yard(const std::vector<std::string>& tokens) {  
-    std::vector<std::string>output;      //string vector
-    std::stack<std::string>ops;         //stack
+  if (!current.empty()) { // for leftovers to push to tokens
+    tokens.push_back(current);
+  }
 
-    for (const auto& t : tokens) {
+  return tokens;
+}
+
+std::vector<std::string> shuting_yard(const std::vector<std::string> &tokens) {
+  std::vector<std::string> output; // string vector
+  std::stack<std::string> ops;     // stack
+
+  for (const auto &t : tokens) {
 
     if (std::isdigit(t[0])) {
-        output.push_back(t);
+      output.push_back(t);
     }
 
-    else if (t == "(") {      //if its a ( then just go to ops
-        ops.push(t);
+    else if (t == "(") { // if its a ( then just go to ops
+      ops.push(t);
     }
 
-    else if (t == ")") {       // put the stuff in parenthesses to the output  by checking for ) then loops every thing before (
-        while (!ops.empty() && ops.top() != "(") {
-            output.push_back(ops.top());
-            ops.pop();
-        }
+    else if (t == ")") { // put the stuff in parenthesses to the output  by
+                         // checking for ) then loops every thing before (
+      while (!ops.empty() && ops.top() != "(") {
+        output.push_back(ops.top());
         ops.pop();
+      }
+      ops.pop();
     }
 
     else if (is_operator(t)) {
-            if (isltr(t)) {     //check if its left to right operation if it is then just loop normally
-            while (!ops.empty() &&
-                   ops.top() != "(" &&
-                   precedence(ops.top()) >= precedence(t)) {      //check if its empty and if its a higher weighted operation
-                output.push_back(ops.top());       //if it is then push to output
-                ops.pop();             
-            }
-        } else { // because of ltr condition if its both expo then it just gets put into the stack 
-            while (!ops.empty() &&  // reason being ex 2^3^4+2. the parenthesses gets push to the stack and + so both of them go to the output but the output os now 2 3 4 ^ ^ + and now its correct order instead of 2 3 ^ 4 ^
-                   ops.top() != "(" &&
-                   precedence(ops.top()) > precedence(t)) {         
-                output.push_back(ops.top());
-                ops.pop();
-            }
+      if (isltr(t)) { // check if its left to right operation if it is then just
+                      // loop normally
+        while (!ops.empty() && ops.top() != "(" &&
+               precedence(ops.top()) >=
+                   precedence(t)) { // check if its empty and if its a higher
+                                    // weighted operation
+          output.push_back(ops.top()); // if it is then push to output
+          ops.pop();
         }
+      } else { // because of ltr condition if its both expo then it just gets
+               // put into the stack
+        while (!ops.empty() && // reason being ex 2^3^4+2. the parenthesses gets
+                               // push to the stack and + so both of them go to
+                               // the output but the output os now 2 3 4 ^ ^ +
+                               // and now its correct order instead of 2 3 ^ 4 ^
+               ops.top() != "(" && precedence(ops.top()) > precedence(t)) {
+          output.push_back(ops.top());
+          ops.pop();
+        }
+      }
 
-        ops.push(t); //for the 1st symbol to be added
+      ops.push(t); // for the 1st symbol to be added
     }
-}
+  }
 
-while (!ops.empty()) {
+  while (!ops.empty()) {
     output.push_back(ops.top());
     ops.pop();
+  }
+
+  return output;
 }
+double evaluvation(const std::vector<std::string> &tokens) {
+  std::stack<double> stack; // stack
 
-return output;
-}
-double evaluvation (const std::vector<std::string>& tokens) {
-    std::stack<double> stack; //stack
-
-    for (const auto& t : tokens) {
-         if (std::isdigit(static_cast<unsigned char>(t[0]))) {    //is digit can only check for unsigned char so we just change its type to that
-            stack.push(std::stod(t));   //push to stack
-        } else {
-            if (t == "sqrt") {
-                double lhs = stack.top(); stack.pop();
-                stack.push(std::sqrt(lhs));
-                continue;
-            }
-            double lhs = stack.top(); stack.pop();   //left hand side
-            double rhs = stack.top(); stack.pop();   //right hand side
-            double result = 0;
-            if (t == "*") result = lhs * rhs;
-            else if (t == "+") result = lhs + rhs;
-            else if (t == "-") result = rhs - lhs;
-            else if (t == "/") result = rhs / lhs;
-            else if (t == "^") result = std::pow (rhs, lhs);
-            stack.push(result);  // push the result so we can continue finding vlaues
-
-        }
+  for (const auto &t : tokens) {
+    if (std::isdigit(static_cast<unsigned char>(
+            t[0]))) { // is digit can only check for unsigned char so we just
+                      // change its type to that
+      stack.push(std::stod(t)); // push to stack
+    } else {
+      if (t == "sqrt") {
+        double lhs = stack.top();
+        stack.pop();
+        stack.push(std::sqrt(lhs));
+        continue;
+      }
+      double lhs = stack.top();
+      stack.pop(); // left hand side
+      double rhs = stack.top();
+      stack.pop(); // right hand side
+      double result = 0;
+      if (t == "*")
+        result = lhs * rhs;
+      else if (t == "+")
+        result = lhs + rhs;
+      else if (t == "-")
+        result = rhs - lhs;
+      else if (t == "/")
+        result = rhs / lhs;
+      else if (t == "^")
+        result = std::pow(rhs, lhs);
+      stack.push(result); // push the result so we can continue finding vlaues
     }
-    return stack.top();  //return the value
+  }
+  return stack.top(); // return the value
 }
